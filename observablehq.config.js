ab6a7ccf9e98563d0ecadea2a574b2c52becd195
ldeso <dev@leodesouza.net>
2025-03-27 08:08:10 +0100

    Update favicon

--- observablehq.config.js
+++ observablehq.config.js
@@ -3,9 +3,10 @@ import MarkdownItFootnote from "markdown-it-footnote";
 export default {
   title: "Klima Visualisations",
   head: `
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">`,
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">`,
   root: "src",
   style: "style.css",
   globalStylesheets: ["https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"],
--- src/index.md
+++ src/index.md
@@ -1,9 +1,10 @@
 ---
 head: |
   <link rel="canonical" href="https://klima.leodesouza.net/whitepaper">
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <meta name="robots" content="noindex,noarchive">
   <meta http-equiv="refresh" content="0; url=./whitepaper">
 ---
